6942fd29dd410527ec8eb8bb0670ee9c685099f8
Markus Machat <83303245+trexxak@users.noreply.github.com>
2025-06-04 00:31:32 +0200

    Improve interpreter token detection

--- interpreter_example.py
+++ interpreter_example.py
@@ -6,15 +6,43 @@ def interpret(text):
     buf = ''
     i = 0
     while i < len(text):
-        c = text[i]
-        if c == '|' and (i == 0 or text[i-1] != '\\'):
+        # Longest symbolic tokens first, including optional inversion prefix '-'
+        if text[i:i+4] in ('-!?|', '-?!|') and (i == 0 or text[i-1] != '\\'):
+            if buf.strip():
+                print('  ' * depth + buf.strip())
+                buf = ''
+            print('  ' * depth + text[i:i+4])
+            depth += 1
+            i += 4
+        elif text[i:i+3] in ('!?|', '?!|') and (i == 0 or text[i-1] != '\\'):
+            if buf.strip():
+                print('  ' * depth + buf.strip())
+                buf = ''
+            print('  ' * depth + text[i:i+3])
+            depth += 1
+            i += 3
+        elif text[i:i+2] in ('!|', '?|') and (i == 0 or text[i-1] != '\\'):
+            if buf.strip():
+                print('  ' * depth + buf.strip())
+                buf = ''
+            print('  ' * depth + text[i:i+2])
+            depth += 1
+            i += 2
+        elif text[i] == '|' and (i == 0 or text[i-1] != '\\'):
             if buf.strip():
                 print('  ' * depth + buf.strip())
                 buf = ''
             print('  ' * depth + '|')
             depth += 1
             i += 1
-        elif c == '_' and i + 1 < len(text) and text[i+1] == '|' and (i == 0 or text[i-1] != '\\'):
+        elif text[i:i+3] in ('_|!', '_|?') and (i == 0 or text[i-1] != '\\'):
+            if buf.strip():
+                print('  ' * depth + buf.strip())
+                buf = ''
+            depth -= 1
+            print('  ' * depth + text[i:i+3])
+            i += 3
+        elif text[i:i+2] == '_|' and (i == 0 or text[i-1] != '\\'):
             if buf.strip():
                 print('  ' * depth + buf.strip())
                 buf = ''
@@ -22,7 +50,7 @@ def interpret(text):
             print('  ' * depth + '_|')
             i += 2
         else:
-            buf += c
+            buf += text[i]
             i += 1
     if buf.strip():
         print(buf.strip())
